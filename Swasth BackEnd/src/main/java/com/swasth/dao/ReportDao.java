8a038065af80512e3b7bc23a5cc3e467a3925bc4
Sarvesh-2732 <sarveshgadkar2732@gmail.com>
2025-02-13 23:40:01 +0530

    re-commiting

--- Swasth BackEnd/src/main/java/com/swasth/dao/ReportDao.java
+++
@@ -1,9 +0,0 @@
-package com.swasth.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.swasth.pojos.Reports;
-
-public interface ReportDao extends JpaRepository<Reports, Integer> {
-
-}
